9a3c2a36eda555ee0f47c736ab0e541e580d0a09
Ricardo Boss <mail@ricardoboss.de>
2025-01-09 10:43:11 +0100

    Add version to HTTP client factory in the User-Agent header

--- packages/microsoft_kiota_http/lib/src/kiota_client_factory.dart
+++ packages/microsoft_kiota_http/lib/src/kiota_client_factory.dart
@@ -3,21 +3,27 @@ part of '../microsoft_kiota_http.dart';
 class KiotaClientFactory {
   KiotaClientFactory._();
 
-  static http.Client createClient() {
-    return retry.RetryClient(
-      http.Client(),
-      when: (response) {
-        const retryCodes = {
-          408, // Request Timeout
-          429, // Too Many Requests
-          500, // Internal Server Error
-          502, // Bad Gateway
-          503, // Service Unavailable
-          504, // Gateway Timeout
-        };
+  static const String _productName = 'kiota-dart';
+  static const String _productVersion = '0.0.1'; // x-release-please-version
 
-        return retryCodes.contains(response.statusCode);
-      },
+  static http.Client createClient() {
+    return UserAgentClient(
+      retry.RetryClient(
+        http.Client(),
+        when: (response) {
+          const retryCodes = {
+            408, // Request Timeout
+            429, // Too Many Requests
+            500, // Internal Server Error
+            502, // Bad Gateway
+            503, // Service Unavailable
+            504, // Gateway Timeout
+          };
+
+          return retryCodes.contains(response.statusCode);
+        },
+      ),
+      userAgent: '$_productName/$_productVersion',
     );
   }
 }
