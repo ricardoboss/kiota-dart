2858a396f906f1c657de48a4af54938340dc3f05
Ricardo Boss <mail@ricardoboss.de>
2025-02-19 01:27:59 +0100

    feat: New OAuth package

---
+++ packages/microsoft_kiota_oauth/lib/microsoft_kiota_oauth.dart
@@ -0,0 +1,7 @@
+﻿/// This library implements an OAuth authentication provider for the
+/// [`microsoft_kiota_abstractions`](https://pub.dev/packages/microsoft_kiota_abstractions) package.
+library microsoft_kiota_oauth;
+
+import 'package:microsoft_kiota_abstractions/microsoft_kiota_abstractions.dart';
+
+part 'src/oauth_authentication_provider.dart';
---
+++ packages/microsoft_kiota_oauth/lib/src/oauth_authentication_provider.dart
@@ -0,0 +1,13 @@
+﻿part of '../microsoft_kiota_oauth.dart';
+
+/// An authentication provider that uses OAuth to authenticate requests.
+class OAuthAuthenticationProvider implements AuthenticationProvider {
+  @override
+  Future<void> authenticateRequest(
+    RequestInformation request, [
+    Map<String, Object>? additionalAuthenticationContext,
+  ]) {
+    // TODO: implement authenticateRequest
+    throw UnimplementedError();
+  }
+}
